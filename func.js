25f8d617f37d7051f2bc45feb778fbf5a7997779
Keito Watanabe <44758524+kate0223@users.noreply.github.com>
2021-11-29 00:37:57 +0900

    Update func.js

--- func.js
+++ func.js
@@ -2,10 +2,12 @@ var i = 0;
 var images = [];
 var slideTime = 3000; // 3 seconds
 
-images[0] = 'https://github.com/kate0223/webclock/blob/main/background_img.jpg?raw=true';
-images[1] = 'https://i.imgur.com/809dZ0J.png';
-images[2] = 'https://i.imgur.com/6r5PQq0.jpeg';
-
+images[0] = 'https://github.com/kate0223/webclock/blob/main/img/andre-benz-JnB8Gio4GZo-unsplash.jpg?raw=true';
+images[1] = 'https://github.com/kate0223/webclock/blob/main/img/andrew-neel-jtsW--Z6bFw-unsplash.jpg?raw=true';
+images[2] = 'https://github.com/kate0223/webclock/blob/main/img/john-towner-JgOeRuGD_Y4-unsplash.jpg?raw=true';
+images[3] = 'https://github.com/kate0223/webclock/blob/main/img/nick-nice-gPm8h3DS1s4-unsplash.jpg?raw=true';
+images[4] = 'https://github.com/kate0223/webclock/blob/main/img/olivier-guillard-xnGu_KGrqX8-unsplash.jpg?raw=true';
+images[5] = 'https://github.com/kate0223/webclock/blob/main/img/steve-harvey-U4wcrDteZ2Y-unsplash.jpg?raw=true';
 function changePicture() {
     document.body.style.backgroundImage = "url(" + images[i] + ")";
 
